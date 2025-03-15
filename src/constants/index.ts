85f6135d87e9356dfc9d273d6e267520e946ee0b
YassirJr <yassir.junior.03@gmail.com>
2025-03-15 23:09:06 +0000

    add id

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,8 @@
-import { Field } from "@/types"
+import {Field} from "@/types"
 
 export const signInFields: Field[] = [
     {
+        id : 1,
         name: "email",
         type: "email",
         label: "Email",
@@ -10,17 +11,61 @@ export const signInFields: Field[] = [
         description: "We'll never share your email with anyone else."
     },
     {
+        id : 2,
         name: "password",
         type: "password",
         label: "Password",
         placeholder: "Password",
         required: true,
         description: "Password must be at least 8 characters long."
+    },
+    {
+        id : 3,
+        name: "region",
+        type: "select",
+        label: "Region",
+        placeholder: "Your region",
+        required: false,
+        description: "Your region",
+        options: [
+            {
+                value: "us",
+                label: "United States"
+            },
+            {
+                value: "ca",
+                label: "Canada"
+            }
+        ]
+    },
+    {
+        id : 4,
+        name : "interests",
+        type: "checkbox",
+        label: "Interests",
+        placeholder: "Interests",
+        required: false,
+        description: "Select your interests",
+        options: [
+            {
+                value: "sports",
+                label: "Sports"
+            },
+            {
+                value: "music",
+                label: "Music"
+            },
+            {
+                value: "movies",
+                label: "Movies"
+            }
+        ]
     }
 ]
 
 export const signUpFields: Field[] = [
     {
+        id : 1,
         name: "name",
         type: "text",
         label: "Full Name",
